6c80f60814819626c73b9ee3640d8a51b9853883
lemon <lemon.listry@gmail.com>
2012-06-11 02:02:58 +0400

    back up

--- webs/data/adminuser.php
+++ webs/data/adminuser.php
@@ -1,6 +1,6 @@
 <?php
 $adminname='admin';
 $adminpwd='647377505c7d4f6507c0c88dba4c89d6';
-$logtime='2012-06-08 19:39:38';
-$logip='183.11.23.252';
+$logtime='2012-06-10 22:08:22';
+$logip='113.92.189.221';
 ?>
