77b5def565619ecab647030f99239f2c5130e3e4
lemon <lemon.listry@gmail.com>
2013-01-11 02:18:45 +0300

    back up

--- mm/protected/commands/TestCommand.php
+++ mm/protected/commands/TestCommand.php
@@ -60,7 +60,7 @@ class TestCommand extends CConsoleCommand{
     //惊艳美女
     public function actionJYurl()
     {
-        for($i=1;$i<42;$i++)
+        for($i=28;$i<42;$i++)
         {
             if($i==1)
                 $url = 'http://www.22mm.cc/mm/jingyan/index.html';
@@ -71,7 +71,6 @@ class TestCommand extends CConsoleCommand{
             preg_match_all('/<a href="(.*)" title="(.*)" target="_blank"><img border="0" src="(.*)">.*<\/a>/iUs', $page,$href);
             
              $dir = Yii::app()->basePath . '/../uploadfile/small/2013/01/';
-            print_r($href);exit;
             foreach($href[2] as $key=>$val)
             {
                 if($key==0)
@@ -84,7 +83,7 @@ class TestCommand extends CConsoleCommand{
                 ob_end_clean();
                 $size = strlen($img);
                 try {
-                    $rand = rand(100000,999999);
+                    $rand = rand(10000000,99999999);
                     $fp2 = @fopen($dir.$rand.'.'.$arr[3] , "a");
                     @fwrite($fp2, $img);
                     @fclose($fp2);
@@ -116,7 +115,6 @@ class TestCommand extends CConsoleCommand{
             preg_match_all('/<a href="(.*)" title="(.*)" target="_blank"><img border="0" src="(.*)">.*<\/a>/iUs', $page,$href);
             
              $dir = Yii::app()->basePath . '/../uploadfile/small/2013/01/';
-            print_r($href);exit;
             foreach($href[2] as $key=>$val)
             {
                 if($key==0)
@@ -129,7 +127,7 @@ class TestCommand extends CConsoleCommand{
                 ob_end_clean();
                 $size = strlen($img);
                 try {
-                    $rand = rand(100000,999999);
+                    $rand = rand(10000000,99999999);
                     $fp2 = @fopen($dir.$rand.'.'.$arr[3] , "a");
                     @fwrite($fp2, $img);
                     @fclose($fp2);
@@ -161,7 +159,6 @@ class TestCommand extends CConsoleCommand{
             preg_match_all('/<a href="(.*)" title="(.*)" target="_blank"><img border="0" src="(.*)">.*<\/a>/iUs', $page,$href);
             
              $dir = Yii::app()->basePath . '/../uploadfile/small/2013/01/';
-            print_r($href);exit;
             foreach($href[2] as $key=>$val)
             {
                 if($key==0)
@@ -174,7 +171,7 @@ class TestCommand extends CConsoleCommand{
                 ob_end_clean();
                 $size = strlen($img);
                 try {
-                    $rand = rand(100000,999999);
+                    $rand = rand(10000000,99999999);
                     $fp2 = @fopen($dir.$rand.'.'.$arr[3] , "a");
                     @fwrite($fp2, $img);
                     @fclose($fp2);
@@ -191,8 +188,10 @@ class TestCommand extends CConsoleCommand{
         }
         
     }
+	
 
-    public function actionInfo()
+
+	public function actionInfo()
     {
         $row = Yii::app()->db->createCommand("select * from url where is_ok=0")->queryAll();
         foreach($row as $v)
@@ -200,36 +199,83 @@ class TestCommand extends CConsoleCommand{
             $url = "http://www.22mm.cc".$v['url'];
             echo $url."\n";
             $page = $this->getUrldata($url);
-            $arr = explode('<script>var arrayImg=new Array();',$page);
-            $a = explode('</script>', $arr[1]);
-            preg_match_all('/\"(.*)\"/iUs', $a[0],$href);
-             $dir = Yii::app()->basePath . '/../uploadfile/img/2013/01/';
+            preg_match('/<div class="pagelist">(.*)<\/div>/iUs', $page,$pages);
+           
+            if(!empty($pages))
+            {
+                 $p = explode('</a>', $pages[1]);
+                 $total = count($p)-2;
+                 for($j=1;$j<=$total;$j++)
+                 {
+                     if($j==1)
+                         $urls = $url = "http://www.22mm.cc".$v['url'];
+                     else
+			{
+                         $u = explode('.',$v['url']);
+                         $urls = $url = "http://www.22mm.cc".$u[0].'-'.$j.'.html';
+                     }
+                     echo $url."\n";
+                     $page = $this->getUrldata($urls);
+                     $arr = explode('<script>var arrayImg=new Array();',$page);
+                    $a = explode('</script>', $arr[1]);
+                    preg_match_all('/\"(.*)\"/iUs', $a[0],$href);
+                     $dir = Yii::app()->basePath . '/../uploadfile/img/2013/01/';
+
+                     foreach($href[1] as $val)
+                     {
+                        $val = str_replace('big','pic',$val);
+                         $arr = explode('.',$val);
+
+                            ob_start();
+                            @readfile($val);
+                            $img = ob_get_contents();
+                            ob_end_clean();
+                            $size = strlen($img);
+                            try {
+                                $rand = rand(10000000,99999999);
+                                $fp2 = @fopen($dir.$rand.'.'.$arr[3] , "a");
+                                @fwrite($fp2, $img);
+                                @fclose($fp2);
+                            } catch (Exception $exc) {
+                                 continue;
+                            }
+                        $imgurl = '/uploadfile/img/2013/01/'.$rand.'.'.$arr[3];
+                        Yii::app()->db->createCommand("insert into mm_img(infoid,imgurl) values('{$v['infoid']}','{$imgurl}')")->execute();
+                        Yii::app()->db->createCommand("update url set is_ok=1 where id=".$v['id'])->execute();
+                     }
+                 }
+            }
+            else
+            {
+                $arr = explode('<script>var arrayImg=new Array();',$page);
+                $a = explode('</script>', $arr[1]);
+                preg_match_all('/\"(.*)\"/iUs', $a[0],$href);
+                 $dir = Yii::app()->basePath . '/../uploadfile/img/2013/01/';
+
+                 foreach($href[1] as $val)
+                 {
+                    $val = str_replace('big','pic',$val);
+                     $arr = explode('.',$val);
+
+                        ob_start();
+                        @readfile($val);
+                        $img = ob_get_contents();
+                        ob_end_clean();
+                        $size = strlen($img);
+                        try {
+                            $rand = rand(10000000,99999999);
+                            $fp2 = @fopen($dir.$rand.'.'.$arr[3] , "a");
+                            @fwrite($fp2, $img);
+                            @fclose($fp2);
+                        } catch (Exception $exc) {
+                             continue;
+                        }
+                    $imgurl = '/uploadfile/img/2013/01/'.$rand.'.'.$arr[3];
+                    Yii::app()->db->createCommand("insert into mm_img(infoid,imgurl) values('{$v['infoid']}','{$imgurl}')")->execute();
+                    Yii::app()->db->createCommand("update url set is_ok=1 where id=".$v['id'])->execute();
+                 }
+            }
             
-             foreach($href[1] as $val)
-             {
-                 
-                 
-                $val = str_replace('big','pic',$val);
-                 $arr = explode('.',$val);
-                 
-                    ob_start();
-                    @readfile($val);
-                    $img = ob_get_contents();
-                    ob_end_clean();
-                    $size = strlen($img);
-                    try {
-                        $rand = rand(100000,999999);
-                        $fp2 = @fopen($dir.$rand.'.'.$arr[3] , "a");
-                        @fwrite($fp2, $img);
-                        @fclose($fp2);
-                    } catch (Exception $exc) {
-                         continue;
-                    }
-                $imgurl = '/uploadfile/img/2013/01/'.$rand.'.'.$arr[3];
-                Yii::app()->db->createCommand("insert into mm_img(infoid,imgurl) values('{$v['infoid']}','{$imgurl}')")->execute();
-                Yii::app()->db->createCommand("update url set is_ok=1 where id=".$v['id'])->execute();
-             }
-	sleep(rand(0,5));
         }
 
     }
