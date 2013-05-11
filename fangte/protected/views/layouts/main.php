9f491509474edb56efac9b37004c7026ec8f9d4b
lemon <lemon.listry@gmail.com>
2013-05-12 02:06:51 +0400

    back up

--- fangte/protected/views/layouts/main.php
+++ fangte/protected/views/layouts/main.php
@@ -87,6 +87,7 @@ this.setHomePage('http://fangte.8090goto.com');
 <a href='http://www.0371fangte.com'  target=_blank title='郑州方特欢乐世界'>郑州方特欢乐世界</a>
 <a href='http://www.blue-rain.cn' target='_blank' title='三亚湾家庭旅馆'>三亚湾家庭旅馆</a>
 <a href='http://www.nnlxs.com' target='_blank' title='南宁旅行社'>南宁旅行社</a>
+<a href='http://www.itoptrip.com' target='_blank' title='大拇指旅行网'>大拇指旅行网</a>
 </div>
 <div class="bqxx">
 &copy; Copyright 2010-2012 株洲方特欢乐世界 All rights reserved 版权所有，网站预订系统由8090旅游团购网提供技术支持<br/>
